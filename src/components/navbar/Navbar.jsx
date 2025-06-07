40581dae5523ba60a0d7f9a0e45f66a37f30a19b
charanrajb28 <charanrajb282004@gmail.com>
2025-06-07 18:01:57 +0530

    final changes 1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,20 +8,23 @@ import ThemeToggle from "../themeToggle/ThemeToggle";
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.social}>
+      {/* <div className={styles.social}>
         <Image src="/facebook.png" alt="facebook" width={24} height={24} />
         <Image src="/instagram.png" alt="instagram" width={24} height={24} />
         <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} />
-      </div>
-      <div className={styles.logo}>lamablog</div>
-      <div className={styles.links}>
+      </div> */}
+      <div className={styles.logo}>EduMagzine</div>
+      <div></div>
+      <div className={styles.links}><div className={styles.links}>
         <Link href="/" className={styles.link}>Homepage</Link>
         <Link href="/" className={styles.link}>Contact</Link>
         <Link href="/" className={styles.link}>About</Link>
+      </div>
+      <div className={styles.links1}>
         <AuthLinks />
         <ThemeToggle />
-      </div>
+      </div></div>
     </div>
   );
 };
